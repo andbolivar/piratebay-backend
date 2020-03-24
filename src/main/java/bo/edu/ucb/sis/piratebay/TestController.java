48ff471f26cf41b165c9739ff9a6709befa0e173
andbolivar <and.bolivar@gmail.com>
2020-03-23 21:05:16 -0400

    PIPO

---
+++ src/main/java/bo/edu/ucb/sis/piratebay/TestController.java
@@ -0,0 +1,17 @@
+package bo.edu.ucb.sis.piratebay;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> helloworld() {
+        return new ResponseEntity<String>("Hola Mundoo", HttpStatus.OK);
+    }
+}
